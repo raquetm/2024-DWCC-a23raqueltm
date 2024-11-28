67f37550ba3bbf3f8ee9f5b97599fb6eca7221e6
raquetm <a23raqueltm@iessanclemente.net>
2024-11-28 11:02:46 +0100

    Inicio proyecto lista compra

---
+++ T5ejerciciosFormularios/proyectoListCompra/lista.js
@@ -0,0 +1,9 @@
+const itemInput = document.getElementById('itemInput');
+const addButton = document.getElementById('addButton');
+const shoppingList = document.getElementById('shoppingList');
+const clearAllButton = document.getElementById('clearAllbutton');
+
+/* <input type="text" id="itemInput" placeholder="Introduce un elemento"><br />
+    <button id="addButton">Engadir elemento</button>
+    <ul id="shoppingList"></ul>
+    <button id="clearAllButton" style="display: none;">Borrar todo</button> */
